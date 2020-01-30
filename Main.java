2c335fa589259ddaeb3e48818950ff208f382080
hibi-yuka <hibikun1998@icloud.com>
2020-01-31 00:03:19 +0900

    test6

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
 package newlang4;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.PushbackReader;
-
 public class Main {
 
 	/**
@@ -20,23 +15,11 @@ public class Main {
 
 	        System.out.println("basic parser");//解析開始
 
-	        String path = "C:\\Users\\c0117312\\Desktop\\text.txt";
-			File file = new File(path);
-			FileReader fr = null;
-			try {
-				fr = new FileReader(file);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-
-			PushbackReader pr = new PushbackReader(fr);
-	        lex = new LexicalAnalyzerImpl(pr); //読み込まれた値や文字がlexへ渡されていく,lexにはLexicalType.○○と字句が渡されてる
-
+	        lex = new LexicalAnalyzerImpl("test.bas");
 	        env = new Environment(lex); //読み込んだものを次にenvへ（ここで渡すのは読み込んだものの）
 
 	        first = lex.get();
 	        lex.unget(first);
-
 
 	        if (Program.isFirst(first)) {//<program>というfirst集合に入っているかを確認する
 
@@ -47,20 +30,6 @@ public class Main {
 	        else System.out.println("syntax error");//firts集合に該当するものなし＝解析できないのでアウト
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //call_subに到達してない
 //print = 1 は　const print 1,print "hello" などはcall_sub = をungetする
